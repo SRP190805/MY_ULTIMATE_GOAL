77063319da19f1c1521804e07f38335442bda305
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2026-01-19 15:59:48 +0530

    STARTING WITH BASH, on my CYBERSEC Journey.

---
+++ arguements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "arguement 1 is $1"
+echo "arguement 2 is $2"
+echo "arguement 3 is $3"
+
+echo "all arguements: $@"
+
---
+++ devdojo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "what is your name? " name
+greeting="hello"
+echo "$greeting $name"
